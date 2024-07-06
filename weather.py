28da527cca793097f9a16a03260e0dabb1cabd4f
fickya1987 <89722540+fickya1987@users.noreply.github.com>
2024-07-06 10:20:22 +0700

    Update weather.py

--- weather.py
+++ weather.py
@@ -9,7 +9,7 @@ def predict_quality(model, df):
     predictions_data = predict_model(estimator = model, data = df)
     return predictions_data['Label'][0]
     
-model = load_model('random_forest_regressor')
+model = load_model('random_forest_regressor.pkl')
 
 
 st.title('Temperature Prediction App')
